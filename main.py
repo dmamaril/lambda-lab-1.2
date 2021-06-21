4702496262e2156c8568ddad53cbfc400024c748
Don Mamaril <donmamaril@Dons-iMac.local>
2021-06-21 11:15:30 -0700

    update

--- main.py
+++ main.py
@@ -43,5 +43,3 @@ aws lambda invoke --function-name student1002-lab2 --log-type Tail \
 --cli-binary-format raw-in-base64-out \
 --payload '{"key1":"value1", "key2":"value2", "key3":"value3"}' \
 outputfile.txt
-
-test
